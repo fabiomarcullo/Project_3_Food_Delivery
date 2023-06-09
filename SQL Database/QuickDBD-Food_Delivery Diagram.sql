0bfa27f71334aa4dfd8a4ff8397917db533a5421
Fabio de Lima <43584153+fabiomarcullo@users.noreply.github.com>
2023-06-09 11:01:40 -0400

    update

---
+++ SQL Database/QuickDBD-Food_Delivery Diagram.sql
@@ -0,0 +1,12 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/XCVyWi
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "Restaurant" (
+    "Restaurant_Name" varchar(255)   NOT NULL,
+    "Rating" BOOLEAN   NOT NULL,
+    "Category" varchar(255)   NOT NULL,
+    "Address" varchar(255)   NOT NULL
+);
+
