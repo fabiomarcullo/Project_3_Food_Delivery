5f05802438a7b3df68163b41775747e069377485
Fabio de Lima <43584153+fabiomarcullo@users.noreply.github.com>
2023-06-11 11:52:41 -0400

    QuickDBD-Food_Delivery_Diagram_update

--- SQL Database/QuickDBD-Food_Delivery Diagram.sql
+++ SQL Database/QuickDBD-Food_Delivery Diagram.sql
@@ -1,10 +1,19 @@
-﻿CREATE TABLE Restaurant (
-  id SERIAL PRIMARY KEY,
-  Restaurant_Name VARCHAR(255) NOT NULL,
-  Rating VARCHAR(5) NOT NULL,
-  Category VARCHAR(255) NOT NULL,
-  Address VARCHAR(255) NOT NULL,
-  Latitude VARCHAR(255) NOT NULL,
-  Longitude VARCHAR(255) NOT NULL,
-  Province VARCHAR(255) NOT NULL
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/XCVyWi
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "Restaurant_Name" (
+    "id" SERIAL   NOT NULL,
+    "restaurant_name" varchar(255)   NOT NULL,
+    "rating" varchar(5)   NOT NULL,
+    "category" varchar(255)   NOT NULL,
+    "address" varchar(255)   NOT NULL,
+    "latitude" varchar(255)   NOT NULL,
+    "longitude" varchar(255)   NOT NULL,
+    "province" varchar(255)   NOT NULL,
+    CONSTRAINT "pk_Restaurant_Name" PRIMARY KEY (
+        "id"
+     )
 );
+
