5babb890651c4a7a87778052bcc84e49ffc40874
Fabio de Lima <43584153+fabiomarcullo@users.noreply.github.com>
2023-06-13 16:34:49 -0400

    Squashed commit of the following:
    commit 245ac577bb81595c43544f9bc7d8bd315fc7b77f
    Author: toejo <toki.arambulo@gmail.com>
    Date:   Tue Jun 13 15:48:41 2023 -0400
    
        update
    
    commit 5c71a27b0a0f963f77fdf76bb0d3b47b8e7b2630
    Author: toejo <toki.arambulo@gmail.com>
    Date:   Tue Jun 13 14:57:30 2023 -0400
    
        update
    
    commit fa2a3222021f31b8d27f29b0a96cd0f587883e47
    Author: toejo <toki.arambulo@gmail.com>
    Date:   Tue Jun 13 00:19:37 2023 -0400
    
        update
    
    commit 1584ab669a6d61d0e93aa4e2538d5f01574a2b35
    Author: toejo <toki.arambulo@gmail.com>
    Date:   Sun Jun 11 17:26:50 2023 -0400
    
        update
    
    commit bfa617e0b62e8560960b54e972954ec52bb651ff
    Author: toejo <toki.arambulo@gmail.com>
    Date:   Sun Jun 11 03:54:39 2023 -0400
    
        web app update
    
    commit 408760ec399aa3c972f005404a6bc49c444ed08c
    Author: toejo <toki.arambulo@gmail.com>
    Date:   Sat Jun 10 23:36:46 2023 -0400
    
        working on web app
    
    commit d7a4e3d9e6a7b3bc9e7e00467d295c03a09a77b5
    Author: Fabio de Lima <43584153+fabiomarcullo@users.noreply.github.com>
    Date:   Fri Jun 9 09:04:10 2023 -0400
    
        Revert "update"
    
        This reverts commit 89768b35f8e471d366cc96bfe8355e8612ab954b.

---
+++ WebApp/static/js/logic.js
@@ -0,0 +1,162 @@
+url = "/restaurants"
+
+function init() {
+  // Grab a reference to the dropdown select elements
+  var selector1 = d3.select("#selDatasetprovince");
+  var selector2 = d3.select("#selDatasetcategory");
+  var selector3 = d3.select("#selDatasetrating");
+
+  selector1.on("change", optionChanged);
+  selector2.on("change", optionChanged);
+  selector3.on("change", optionChanged);
+
+  // Use the list of unique values from app.json to populate the select options
+  d3.json(url).then((data) => {
+    var provinces = Array.from(new Set(data.map((d) => d.province))).sort();
+    var categories = Array.from(new Set(data.map((d) => d.category))).sort();
+    var ratings = Array.from(new Set(data.map((d) => d.rating))).sort();
+
+    provinces.forEach((province) => {
+      selector1.append("option").text(province).property("value", province);
+    });
+
+    categories.forEach((category) => {
+      selector2.append("option").text(category).property("value", category);
+    });
+
+    ratings.forEach((rating) => {
+      selector3.append("option").text(rating).property("value", rating);
+    });
+
+    // Use the first sample from the list to build the initial plots
+    var firstSample = provinces[0];
+    buildCharts(firstSample);
+    buildMetadata(firstSample);
+  });
+}
+
+// Initialize the dashboard
+init();
+
+function optionChanged() {
+  var selectedProvince = d3.select("#selDatasetprovince").property("value");
+  var selectedCategory = d3.select("#selDatasetcategory").property("value");
+  var selectedRating = d3.select("#selDatasetrating").property("value");
+
+  // Pass the selected values to the filter function
+  filterData(selectedProvince, selectedCategory, selectedRating);
+}
+
+function filterData(province, category, rating) {
+  d3.json(url).then((data) => {
+    var filteredData = data;
+
+    // Apply filters only if a value is selected
+    if (province) {
+      filteredData = filteredData.filter((d) => d.province === province);
+    }
+
+    if (category) {
+      filteredData = filteredData.filter((d) => d.category === category);
+    }
+
+    if (rating) {
+      filteredData = filteredData.filter((d) => d.rating === rating);
+    }
+
+    // Pass the filtered data to the visualization functions
+    buildMetadata(filteredData);
+    buildMarkers(filteredData);
+  });
+}
+
+function buildMetadata(filteredData) {
+  var PANEL = d3.select("#sample-metadata");
+  PANEL.html("");
+
+  if (filteredData.length === 0) {
+    PANEL.append("h6").text("No information available for this selection");
+    return; // Exit the function if no match is found
+  }
+
+  // Create a table element
+  var table = PANEL.append("table");
+
+  // Create the table header row
+  var thead = table.append("thead");
+  var headerRow = thead.append("tr");
+
+  // Add the column name to the table header row
+  headerRow.append("th").text("Restaurant Name");
+
+  // Create the table body
+  var tbody = table.append("tbody");
+
+  // Add rows and cells to the table body
+  filteredData.forEach(function (row) {
+    var dataRow = tbody.append("tr");
+
+    // Add the restaurant name to the cell
+    dataRow.append("td").text(row.restaurant_name);
+  });
+}
+
+function buildMarkers(filteredData) {
+  // Clear existing markers from the map
+  markerCluster.clearLayers();
+
+  // Iterate over the filtered data and create markers for each restaurant
+  filteredData.forEach((restaurant) => {
+    var marker = L.marker([restaurant.latitude, restaurant.longitude]).bindPopup(
+      "<strong>" +
+        restaurant.restaurant_name +
+        "</strong><br />" +
+        "Category: " +
+        restaurant.category +
+        "<br />" +
+        "Rating: " +
+        restaurant.rating +
+        "/5"
+    );
+
+    markerCluster.addLayer(marker); // Add the marker to the marker cluster group
+  });
+}
+
+// Creating the map object
+var myMap = L.map("map", {
+  center: [43.6532, -79.3832], // Toronto GTA coordinates: [latitude, longitude]
+  zoom: 10, // Adjust the zoom level as needed
+});
+
+// Adding the tile layer
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(myMap);
+
+// Cluster marker
+var markerCluster = L.markerClusterGroup();
+
+// Load the data from app.json
+d3.json(url).then((data) => {
+  // Process the retrieved data
+  // Iterate over the data and create markers for each restaurant
+  data.forEach((restaurant) => {
+    var marker = L.marker([restaurant.latitude, restaurant.longitude]).bindPopup(
+      "<strong>" +
+        restaurant.restaurant_name +
+        "</strong><br />" +
+        "Category: " +
+        restaurant.category +
+        "<br />" +
+        "Rating: " +
+        restaurant.rating +
+        "/5"
+    );
+
+    markerCluster.addLayer(marker); // Add the marker to the marker cluster group
+  });
+
+  myMap.addLayer(markerCluster); // Add the marker cluster group to the map
+});
