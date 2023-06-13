fa2a3222021f31b8d27f29b0a96cd0f587883e47
toejo <toki.arambulo@gmail.com>
2023-06-13 00:19:37 -0400

    update

--- WebApp/app.py
+++ WebApp/app.py
@@ -1,33 +1,70 @@
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, inspect
+
 import requests
 from flask import Flask, jsonify, render_template
 
-url = 'https://gbfs.citibikenyc.com/gbfs/en/station_information.json'
 
+# Database setup
+engine = create_engine("postgresql+psycopg2://knskdyld:qfGxP5LoTlErHnHlhVsayMFEmaXV_B-R@hansken.db.elephantsql.com/knskdyld")
+
+# reflect an existing database into a new model
+Base = automap_base()
+
+# reflect the tables
+Base.prepare(autoload_with=engine)
+
+## To gain information form the database
+
+## Base.classes.keys()
+## inspector = inspect(engine)
+## columns = inspector.get_columns('restaurant')
+
+# Saving reference to the table
+restaurants = Base.classes.restaurant
+
+session = Session(engine)
+
+# Flask setup
 app = Flask(__name__)
 
-@app.route("/")
-def index ():
-    return render_template("index.html")
+# Connecting to index.html
+# @app.route("/")
+# def index ():
+#     return render_template("index.html")
 
-@app.route("/api")
-def api():
+# Define a route for retrieving the restaurant data as JSON
+@app.route('/restaurants')
+def get_restaurants():
+    results = session.query(
+        restaurants.restaurant_name,
+        restaurants.rating,
+        restaurants.category,
+        restaurants.address,
+        restaurants.latitude,
+        restaurants.longitude,
+        restaurants.province
+    ).all()
 
-    response = requests.get(url)
+    # Convert the results to a list of dictionaries
+    restaurant_list = []
+    for result in results:
+        restaurant = {
+            'restaurant_name': result.restaurant_name,
+            'rating': result.rating,
+            'category': result.category,
+            'address': result.address,
+            'latitude': result.latitude,
+            'longitude': result.longitude,
+            'province': result.province
+        }
+        restaurant_list.append(restaurant)
 
-    if response.status_code == 200:
-
-        data = response.json()
-
-        return jsonify(data)
+    # Return the restaurant data as JSON
+    return jsonify(restaurant_list)
     
-    else:
-        
-        return jsonify({
-            "error": "failed to get data"
-        })
-
-    return 0
-
 
 if __name__ == "__main__":
     app.run()
