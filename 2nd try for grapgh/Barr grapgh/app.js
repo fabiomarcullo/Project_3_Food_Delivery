e34238f06b5f34f7c3ea8c30696e38484ecd29bb
RahiqO <Rahiq.a.osman@gmail.com>
2023-06-14 21:36:03 -0400

    Bar graph with a bug

--- 2nd try for grapgh/Barr grapgh/app.js
+++ 2nd try for grapgh/Barr grapgh/app.js
@@ -1,34 +1,35 @@
-fetch('restaurant.json')
+function bar_data(sample_no) { fetch('restaurant.json')
   .then(response => response.json())
-  .then(data => { console.log(data)})
+  .then(data => {
+    // { console.log(data)})
 
-// filter_provinces = data.filter(item => item.province);
-// filter_cuisines = data.filter(item => item.category)
-
-
-// let trace1 = {
-//   x: filter_provinces,
-//   y: filter_cuisines,
-//   text: labels_slicedData,
-//   orientation: "h",
-//   type: "bar"
-// };
+  filter_provinces = data.filter(item => item.province == province)
+  filter_cuisines = data.filter(item => item.category == category)
 
 
-// let traceData = [trace1];
-
-// let layout = {
-//   title: "horizontal bar chart ",
-//   margin: {
-//     l: 100,
-//     r: 100,
-//     t: 100,
-//     b: 100
-//   }
-
-// }
+let trace1 = {
+  x: filter_provinces,
+  y: filter_cuisines,
+  text: labels_slicedData,
+  orientation: "h",
+  type: "bar"
+};
 
 
-// Plotly.newPlot("bar", traceData, layout);
-// })
-// }
+let traceData = [trace1];
+
+let layout = {
+  title: "horizontal bar chart ",
+  margin: {
+    l: 100,
+    r: 100,
+    t: 100,
+    b: 100
+  }
+
+}
+
+
+Plotly.newPlot("bar", traceData, layout);
+})
+}
